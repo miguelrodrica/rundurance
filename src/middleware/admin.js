9753fb6f282ce8f1333c51fdec6010b454176ecc
miguelrodrica <mangelrodca@gmail.com>
2026-08-07 21:56:55 -0500

    chore(clean): limpieza definitiva, solo README de aclaración en main/dev

--- src/middleware/admin.js
+++
@@ -1,8 +0,0 @@
-function admin(req, res, next) {
-  if (req.trainer.role !== 'admin') {
-    return res.status(403).json({ error: 'Acceso restringido a administradores' });
-  }
-  next();
-}
-
-module.exports = admin;
